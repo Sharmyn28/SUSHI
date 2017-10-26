913e690d3b0b59012817938e93e86e058a2e3671
dianamariasilva <diana.silva.v94@gmail.com>
2017-10-26 16:47:49 -0500

    carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -36,69 +36,75 @@ class Carousel extends Component {
 								<h2>Organic greens topped with fresh sashimi, wasabi soy vinaigrette.<br />
 								</h2>
 								<p>
-								Organic greens topped with fresh sashimi, wasabi soy vinaigrette.
+									Organic greens topped with fresh sashimi, wasabi soy vinaigrette.
                 </p>
-								<a
-									className="btn btn-info pull-right"
-									href="/article/show/sed-vel-lectus/9">
-									<i className="fa fa-long-arrow-right">
-										+ADD TO CART
-									</i>
-								</a>
-							</div>
-							<div className="col-lg-3">
-								<h5>nutritional Info</h5>
-								<ul>
-									<li></li>
-								</ul>
-						</div>
-						<div className="item">
-							<div className="col-lg-8 pull-right">
-								<img src="http://lorempixel.com/g/750/350/cats" />
-							</div>
-							<div className="col-lg-4">
-								<h2>
-									Proin porta auctor nisi<br />
-									<small>
-										by Merovingio in Jun 22, 2017 at
-                    00:34
-</small>
-								</h2>
-								<p>
-									Proin porta auctor nisi in interdum. Praesent a accumsan neque. Quisque ut nulla ac libero egestas tristique. Nunc venenatis elit lorem, ut viverra neque rhoncus a. Proin erat risus, pharetra vitae elementum eget, accumsan ornare mauris.
-                  Praesent...
-                </p>
-								<a
-									className="btn btn-info pull-right"
-									href="/article/show/sed-vel-lectus/9">
-									<i className="fa fa-long-arrow-right">
-									</i>
-								</a>
+								<hr/>
+								<div className="row">
+									<div className="col-lg-6">
+											$ 12.00
+									</div>
+									<div className="col-lg-6">
+											<a
+											className="btn btn-info pull-right"
+											href="/article/show/sed-vel-lectus/9">
+												<i className="fa fa-long-arrow-right">
+												</i>
+											</a>
+									</div>
+								</div>
 							</div>
 						</div>
 						<div className="item">
-							<div className="col-lg-8 pull-right">
-								<img src="http://lorempixel.com/g/750/350/fashion" />
+							<div className="col-lg-3 pull-right">
+								<img src="http://demos.telerik.com/kendo-ui/content/spa/websushi/images/200/sashimi-salad.jpg" />
 							</div>
-							<div className="col-lg-4">
-								<h2>
-									Aenean sodales<br />
-									<small>
-										by Merovingio in Jun 22, 2017 at
-                    00:33
-</small>
+							<div className="col-lg-6">
+								<h2>Organic greens topped with fresh sashimi, wasabi soy vinaigrette.<br />
 								</h2>
 								<p>
-									Aenean sodales, leo eu euismod tincidunt, felis odio aliquam velit,
-                  quis porta lorem erat eget erat. Aliquam porta libero erat, sed aliquet est
-                  sollicitudin a. Curabitur nec tellus in eros egestas venenatis ac sed nisl. In consectetur nisl eget...
+									Organic greens topped with fresh sashimi, wasabi soy vinaigrette.
                 </p>
-								<a
-									className="btn btn-info pull-right"
-									href="/article/show/sed-vel-lectus/9">
-									<i className="fa fa-long-arrow-right">
-									</i>
-								</a>
+								
+								<hr/>
+								<div className="row">
+									<div className="col-lg-6">
+											$ 12.00
+									</div>
+									<div className="col-lg-6">
+											<a
+											className="btn btn-info pull-right"
+											href="/article/show/sed-vel-lectus/9">
+												<i className="fa fa-long-arrow-right">
+												</i>
+											</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="item ">
+							<div className="col-lg-3 pull-right">
+								<img src="http://demos.telerik.com/kendo-ui/content/spa/websushi/images/200/sashimi-salad.jpg" />
+							</div>
+							<div className="col-lg-6">
+								<h2>Organic greens topped with fresh sashimi, wasabi soy vinaigrette.<br />
+								</h2>
+								<p>
+									Organic greens topped with fresh sashimi, wasabi soy vinaigrette.
+                </p>
+								<hr/>
+								<div className="row">
+									<div className="col-lg-6">
+											$ 12.00
+									</div>
+									<div className="col-lg-6">
+											<a
+											className="btn btn-info pull-right"
+											href="/article/show/sed-vel-lectus/9">
+												<i className="fa fa-long-arrow-right">
+												</i>
+											</a>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
