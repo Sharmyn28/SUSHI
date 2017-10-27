f5a1a62dfe0729d37b2caaeff228858796dcd24d
evecarina <carina120794@gmail.com>
2017-10-26 20:27:31 -0500

    conflicto

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import Home from './Home';
 import {Provider} from 'redux-zero/react'
 import store from './store'
 import registerServiceWorker from './registerServiceWorker';
-import {HashRouter, Switch, Route} from 'react-router-d
+import {HashRouter, Switch, Route} from 'react-router-dom'
 
 const Index = () => (
    <Provider store={store}>
