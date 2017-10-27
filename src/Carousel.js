0da27c670839da107a6a02eb1e1af394b5fff03b
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-10-26 22:46:44 -0500

    integracion carousel view

--- src/Carousel.js
+++ src/Carousel.js
@@ -4,102 +4,116 @@ import './Carousel.css';
 // import {dishes, selectedFood} from './dishes'
 
 import { connect } from 'redux-zero/react';
-import {nextAction, previewAction} from './actions-diana';
+import { nextAction, previewAction } from './actions-diana';
+import Header from './header';
+import ShoppingCart from './shoppingCart';
 
-const InfoDish = ({ image, name, description,price ,nutritional, nutritionalInfo}) => {
+const InfoDish = ({ image, name, description, price, nutritional, nutritionalInfo }) => {
 	return (
 		<div
-		className="carousel-inner text-center"
-		role="listbox">
-		<div className="item active">
-			<div className="col-lg-3 pull-left">
-				<img src={image} />
-			</div>
-			<div className="col-lg-6">
-				<h2>{name}<br />
-				</h2>
-				<p>{description}</p>
-				<hr />
-				<div className="row">
-					<div className="col-lg-6">
-						{price}
-								</div>
-					<div className="col-lg-6">
-						<a
-							className="btn btn-info pull-right"
-							href="/article/show/sed-vel-lectus/9">
-							<i className="fa fa-long-arrow-right">
-								+ADD TO CART
-											</i>
-						</a>
+			className="carousel-inner text-center"
+			role="listbox">
+			<div className="item active">
+				<div className="col-lg-3 pull-left">
+					<img src={image} />
+				</div>
+				<div className="col-lg-6">
+					<h2>{name}<br />
+					</h2>
+					<p>{description}</p>
+					<hr />
+					<div className="row">
+						<div className="col-lg-6">{price}</div>
+						<div className="col-lg-6">
+							<a
+								className="btn btn-info pull-right"
+								href="/article/show/sed-vel-lectus/9">
+								<i className="fa fa-long-arrow-right">+ADD TO CART</i>
+							</a>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="col-lg-3 pull-right">
-				<h5>{nutritional}</h5>
-				<ul>
-					<li> {nutritionalInfo}</li>
-				</ul>
+				<div className="col-lg-3 pull-right">
+					<h5>{nutritional}</h5>
+					<ul>
+						<li> {nutritionalInfo}</li>
+					</ul>
+				</div>
 			</div>
 		</div>
-	</div>
 	)
 }
 
 
-const Carousel = ({ dishes, selectedFood}) => {
-	console.log("disshes",selectedFood)
+const Carousel = ({ dishes, selectedFood }) => {
+	console.log("disshes", selectedFood)
 	return (
-		<div className="background-fish">
-			<div className="container carousel">
-				<div
-					id="carousel-example-generic"
-					className="carousel slide" data-ride="carousel">
-					
-					<div>
-		<ol className="carousel-indicators">
-			<li
-				data-target="#carousel-example-generic"
-				data-slide-to={selectedFood}
-				className="active" />
-		</ol>
-				{/* Wrapper for slides */ }
-				<InfoDish 
-		  	image={dishes[selectedFood].image}
-				price={dishes[selectedFood].price}
-				description={dishes[selectedFood].description}
-				nutritional = {dishes[selectedFood].nutritional}
-				nutritionalInfo= {dishes[selectedFood].nutritionalInfo}
-			/>
-		<a
-			className="left carousel-control"
-			href="#carousel-example-generic"
-			role="button"
-			data-slide="prev"
-			onClick={previewAction}>
-			<span
-				className="glyphicon glyphicon-chevron-left"
-				aria-hidden="true" />
-			<span className="sr-only">Previous</span>
-		</a>
-		<a
-			className="right carousel-control"
-			href="#carousel-example-generic"
-			role="button"
-			data-slide="next"
-			onClick={nextAction}>
-			<span
-				className="glyphicon glyphicon-chevron-right"
-				aria-hidden="true" />
-			<span className="sr-only">Next</span>
-		</a>
-		</div>
+		<div>
+			<div className="background-fish">
+				<div className="container carousel">
+					<div
+						id="carousel-example-generic"
+						className="carousel slide" data-ride="carousel">
+						<div>
+							<ol className="carousel-indicators">
+								<li
+									data-target="#carousel-example-generic"
+									data-slide-to={selectedFood}
+									className="active" />
+							</ol>
+							{/* Wrapper for slides */}
+							<InfoDish
+								image={dishes[selectedFood].image}
+								price={dishes[selectedFood].price}
+								description={dishes[selectedFood].description}
+								nutritional={dishes[selectedFood].nutritional}
+								nutritionalInfo={dishes[selectedFood].nutritionalInfo}
+							/>
+							<a
+								className="left carousel-control"
+								href="#carousel-example-generic"
+								role="button"
+								data-slide="prev"
+								onClick={previewAction}>
+								<span
+									className="glyphicon glyphicon-chevron-left"
+									aria-hidden="true" />
+								<span className="sr-only">Previous</span>
+							</a>
+							<a
+								className="right carousel-control"
+								href="#carousel-example-generic"
+								role="button"
+								data-slide="next"
+								onClick={nextAction}>
+								<span
+									className="glyphicon glyphicon-chevron-right"
+									aria-hidden="true" />
+								<span className="sr-only">Next</span>
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 	);
-
 }
 
-const mapToProps = ({dishes, selectedFood}) => ({dishes, selectedFood});
-export default connect(mapToProps)(Carousel);
+const CarouselView = ({dishes, selectedFood}) => {
+	return (
+		<div id='application' >
+			<div>
+				<div id='wrapper'>
+					<Header />
+					<div id='main-section'>
+						<ShoppingCart dishes={dishes} />
+						<Carousel dishes={dishes} selectedFood={selectedFood}/>
+					</div>
+				</div>
+			</div>
+		</div >
+	)
+}
+
+const mapToProps = ({ dishes, selectedFood }) => ({ dishes, selectedFood });
+export default connect(mapToProps)(CarouselView);
--- src/Home.js
+++ src/Home.js
@@ -4,34 +4,33 @@ import {connect} from 'redux-zero/react';
 import './App.css';
 
 const Home = ( {dishes}) =>  {
-    return (<section id="main-section">
-        <div>
-            <ul className="k-widget k-listview">
-            {
-                dishes.map((item, index)=>{
-                return(
-                    <li key={index} className="products">
-                    <a className="view-details">
-                        <img className="main-image" src={item.image}/>
-                        <strong>{item.name}</strong>
-                        <span className="price">
-                            <span>$</span>
-                            <span>{item.price}</span>
-                        </span>
-                    </a>
-                    {/* <NavLink className="btn btn-button title" to={playList.ruta}>{playList.title}</NavLink > */}
-                    <button className="add-to-cart">Add to cart</button>
-                    </li>
-                );
-                })
-            }
-            </ul>
-        </div>
-    </section>)
-    }
+  return (<section id="main-section">
+    <div>
+      <ul className="k-widget k-listview">
+        {
+          dishes.map((item, index)=>{
+            return(
+              <li key={index} className="products">
+                <a className="view-details">
+                  <img className="main-image" src={item.image}/>
+                  <strong>{item.name}</strong>
+                  <span className="price">
+  	                <span>$</span>
+    	              <span>{item.price}</span>
+                  </span>
+                </a>
+                {/* <NavLink className="btn btn-button title" to={playList.ruta}>{playList.title}</NavLink > */}
+                <button className="add-to-cart">Add to cart</button>
+              </li>
+            );
+          })
+      	}
+      </ul>
+    </div>
+  </section>)
+}
   
-    /*const mapToProps = ({dishes}) => ({dishes});
-    export default connect(mapToProps)(Home);*/
-
-    export default Home;
+/*const mapToProps = ({dishes}) => ({dishes});
+export default connect(mapToProps)(Home);*/
+export default Home;
   
