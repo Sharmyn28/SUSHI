3076a3bd2e5cbd200d7e32b08f25b1da98a1ca87
aydee <aydee.qm@gmail.com>
2017-10-27 09:09:37 -0500

    agregando count cantidad

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,41 @@
 import React, { Component } from 'react';
 import {connect} from 'redux-zero/react';
 /* import {Redirect, NavLink} from 'react-router-dom';*/
+import {addDishes} from './actions'
 import './App.css';
 
+const Lista_Dishes = ({image, name, price, index}) =>{
+  return(
+        <li className="products">
+        <a className="view-details">
+          <img className="main-image" src={image}/>
+          <strong>{name}</strong>
+          <span className="price">
+            <span>$</span>
+            <span>{price}</span>
+          </span>
+        </a>
+        {/* <NavLink className="btn btn-button title" to={playList.ruta}>{playList.title}</NavLink > */}
+        <button className="add-to-cart"  onClick={() => addDishes(index)}>Add to cart</button>
+      </li>
+  );
+}
+
 const Home = ( {dishes}) =>  {
+
+  const listaComponent = dishes.map((item, index)=>{
+    return <Lista_Dishes
+        key={index}
+        image={item.image} 
+        name={item.name}
+        price={item.price}
+        index={index}
+      />
+  })
   return (<section id="content">
     <div>
       <ul className="k-widget k-listview">
-        {
-          dishes.map((item, index)=>{
-            return(
-              <li key={index} className="products">
-                <a className="view-details">
-                  <img className="main-image" src={item.image}/>
-                  <strong>{item.name}</strong>
-                  <span className="price">
-  	                <span>$</span>
-    	              <span>{item.price}</span>
-                  </span>
-                </a>
-                {/* <NavLink className="btn btn-button title" to={playList.ruta}>{playList.title}</NavLink > */}
-                <button className="add-to-cart">Add to cart</button>
-              </li>
-            );
-          })
-      	}
+        {listaComponent}
       </ul>
     </div>
   </section>)
