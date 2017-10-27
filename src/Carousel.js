b10c16b354202da45940b9268f692926817d7e2d
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-10-26 23:51:32 -0500

    identado

--- src/Carousel.js
+++ src/Carousel.js
@@ -7,102 +7,102 @@ import Header from './header';
 import ShoppingCart from './shoppingCart';
 import Footer from './footer';
 
-const InfoDish = ({ image, name, description,price ,nutritional, nutritionalInfo}) => {
-  return (
-    <div
-    className="carousel-inner text-center"
-    role="listbox">
-    <div className="item active">
-      <div className="col-lg-3 pull-left">
-        <img className="img1" src={image} />
-      </div>
-      <div className="col-lg-6 info-middle">
-        <br/>
-        <h1 className="text-left">{name}</h1>
-        <p className="text-left">{description}</p>
-        <br /><hr/>
-        <div className="row">
-          <div className="col-lg-6 price1">
-            $ {price}.00
+const InfoDish = ({ image, name, description, price, nutritional, nutritionalInfo }) => {
+	return (
+		<div
+			className="carousel-inner text-center"
+			role="listbox">
+			<div className="item active">
+				<div className="col-lg-3 pull-left">
+					<img className="img1" src={image} />
+				</div>
+				<div className="col-lg-6 info-middle">
+					<br />
+					<h1 className="text-left">{name}</h1>
+					<p className="text-left">{description}</p>
+					<br /><hr />
+					<div className="row">
+						<div className="col-lg-6 price1">
+							$ {price}.00
                 </div>
-          <div className="col-lg-6">
-            <a
-              className="btn btn-danger pull-right"
-              href="/article/show/sed-vel-lectus/9">
-              <i className="fa fa-long-arrow-right">
-                +ADD TO CART
+						<div className="col-lg-6">
+							<a
+								className="btn btn-danger pull-right"
+								href="/article/show/sed-vel-lectus/9">
+								<i className="fa fa-long-arrow-right">
+									+ADD TO CART
               </i>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="col-lg-3 pull-right">
-        <h5>{nutritional}</h5>
-        <br/>
-        <ul>
-          <li> {nutritionalInfo}</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  )
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="col-lg-3 pull-right">
+					<h5>{nutritional}</h5>
+					<br />
+					<ul>
+						<li> {nutritionalInfo}</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-const Carousel = ({ dishes, selectedFood}) => {
-  console.log("dishes",selectedFood)
-  return (
-    <div className="background-fish">
-      <div className="container carousel">
-        <div
-          id="carousel-example-generic"
-          className="carousel slide" data-ride="carousel">
-          
-          <div>
-    <ol className="carousel-indicators">
-      <li
-        data-target="#carousel-example-generic"
-        data-slide-to={selectedFood}
-        className="active" />
-    </ol>
-        {/* Wrapper for slides */ }
-        <InfoDish 
-        name={dishes[selectedFood].name}
-        image={dishes[selectedFood].image}
-        price={dishes[selectedFood].price}
-        description={dishes[selectedFood].description}
-        nutritional = {dishes[selectedFood].nutritional}
-        nutritionalInfo= {dishes[selectedFood].nutritionalInfo}
-      />
-    <a
-      className="left carousel-control next1"
-      href="#carousel-example-generic"
-      role="button"
-      data-slide="prev"
-      onClick={previewAction}>
-      <span
-        className="glyphicon glyphicon-chevron-left"
-        aria-hidden="true" />
-      <span className="sr-only">Previous</span>
-    </a>
-    <a
-      className="right carousel-control next2"
-      href="#carousel-example-generic"
-      role="button"
-      data-slide="next"
-      onClick={nextAction}>
-      <span
-        className="glyphicon glyphicon-chevron-right"
-        aria-hidden="true" />
-      <span className="sr-only">Next</span>
-    </a>
-    </div>
-        </div>
-      </div>
-    </div>
-  );
+const Carousel = ({ dishes, selectedFood }) => {
+	console.log("dishes", selectedFood)
+	return (
+		<div className="background-fish">
+			<div className="container carousel">
+				<div
+					id="carousel-example-generic"
+					className="carousel slide" data-ride="carousel">
+
+					<div>
+						<ol className="carousel-indicators">
+							<li
+								data-target="#carousel-example-generic"
+								data-slide-to={selectedFood}
+								className="active" />
+						</ol>
+						{/* Wrapper for slides */}
+						<InfoDish
+							name={dishes[selectedFood].name}
+							image={dishes[selectedFood].image}
+							price={dishes[selectedFood].price}
+							description={dishes[selectedFood].description}
+							nutritional={dishes[selectedFood].nutritional}
+							nutritionalInfo={dishes[selectedFood].nutritionalInfo}
+						/>
+						<a
+							className="left carousel-control next1"
+							href="#carousel-example-generic"
+							role="button"
+							data-slide="prev"
+							onClick={previewAction}>
+							<span
+								className="glyphicon glyphicon-chevron-left"
+								aria-hidden="true" />
+							<span className="sr-only">Previous</span>
+						</a>
+						<a
+							className="right carousel-control next2"
+							href="#carousel-example-generic"
+							role="button"
+							data-slide="next"
+							onClick={nextAction}>
+							<span
+								className="glyphicon glyphicon-chevron-right"
+								aria-hidden="true" />
+							<span className="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-const CarouselView = ({dishes, selectedFood}) => {
+const CarouselView = ({ dishes, selectedFood }) => {
 	return (
 		<div id='application'>
 			<div>
@@ -110,7 +110,7 @@ const CarouselView = ({dishes, selectedFood}) => {
 					<Header />
 					<div id='main-section'>
 						<ShoppingCart dishes={dishes} />
-						<Carousel dishes={dishes} selectedFood={selectedFood}/>
+						<Carousel dishes={dishes} selectedFood={selectedFood} />
 					</div>
 					<Footer />
 				</div>
